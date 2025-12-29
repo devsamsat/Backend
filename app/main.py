53e27d2c6702ec25a987cd0fa1777d5efdeeb6f1
mochlukman <moch.lukman@gmail.com>
2025-12-29 17:13:11 +0700

    Tambahkan Swagger

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from app.interfaces.api import users
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Clean Architecture Users API")
+app = FastAPI(
+    title="Clean Architecture Users API",
+    docs_url="/",
+    redoc_url=None
+)
 
 app.include_router(users.router)
